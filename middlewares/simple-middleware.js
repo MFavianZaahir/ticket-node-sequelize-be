702bb5883ec0d64671c7eca2111119ee387d1aed
MFavian Zaahir <favian.zahir@gmail.com>
2024-05-05 17:50:26 +0700

    i hate my life

---
+++ middlewares/simple-middleware.js
@@ -0,0 +1,8 @@
+const midOne = async (request, response, next) => {
+   console.log(`Run Middleware One`);
+   next(); /** next() function used to continue to the controller process */
+};
+
+module.exports = {
+   midOne
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,16 @@
 const express = require(`express`)
 const app = express()
 app.use(express.json())
+
+const { midOne } = require("../middlewares/simple-middleware")
+const { validateUser } = require("../middlewares/user-validation")
 const userController = require(`../controllers/user.controller`)
+
+app.get("/", [midOne], userController.getAllUser)
+app.post("/", validateUser, userController.addUser)
+/** create route to update user
+* using method "PUT" and define parameter for "id" */
+app.put("/:id", validateUser, userController.updateUser)
 
 app.get("/", userController.getAllUser)
 app.get("/:key", userController.findUser)
